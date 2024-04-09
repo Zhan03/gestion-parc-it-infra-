29d10976122f4522f2d07bb86f2447eb49f00f23
Zhan03 <147081247+Zhan03@users.noreply.github.com>
2024-04-09 14:17:05 +0200

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,55 +1,58 @@
-# main.tf
- 
-# Configuration du provider Azure
-terraform {
+terraform{
+
+
+
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "~> 3.0.0"
     }
   }
+  
   required_version = ">= 0.14.9"
 }
+
 provider "azurerm" {
   features {}
 }
-# Ajouter la documentation pour la ressource random_integer
-# Documentation de la ressource random_integer : https://github.com/hashicorp/terraform-provider-random/blob/main/docs/resources/integer.md
- 
+
 # Génération d'un nombre aléatoire
 resource "random_integer" "random_num" {
   min = 1000
   max = 9999
 }
- 
-# Création du groupe de ressources
+
 resource "azurerm_resource_group" "my_resource_group" {
   name     = "rg-cheung-${random_integer.random_num.result}"
-  location = "francecentral" # Remplacez par votre région Azure préférée
+  location = "francecentral"
 }
- 
-# Création du plan App Service
+
 resource "azurerm_app_service_plan" "my_app_service_plan" {
   name                = "asp-cheung-${random_integer.random_num.result}"
   location            = azurerm_resource_group.my_resource_group.location
   resource_group_name = azurerm_resource_group.my_resource_group.name
- 
+
   sku {
-    tier = "Basic"
+    tier = "B1"
     size = "B1"
   }
+  
+  kind     = "Linux"
+  reserved = true
 }
- 
-# Création de l'application web
+
+
 resource "azurerm_app_service" "my_web_app" {
-  name                = "rg-cheung-${random_integer.random_num.result}"
+  name                = "webapp-cheung-${random_integer.random_num.result}"
   location            = azurerm_resource_group.my_resource_group.location
   resource_group_name = azurerm_resource_group.my_resource_group.name
+
   app_service_plan_id = azurerm_app_service_plan.my_app_service_plan.id
- 
+
   site_config {
-    java_version = "1.8"
-    java_container = "JAVA"
-  
+    java_version        = "1.8"
+    java_container      = "JAVA"
   }
 }
+
+  
